b7b730d18b030f03d604d5ed2c31a7893f44190c
Marvin Banguero <malejandrobanguero@gmail.com>
2026-04-22 10:54:24 -0500

    fix url /

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('kardex/', include('kardex.urls'))
+    path('kardex/', include('kardex.urls')),
+    path('', RedirectView.as_view(url='/kardex/login/', permanent=True)),
 ]
